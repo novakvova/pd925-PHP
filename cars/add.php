32484869ed616086be800667f979a0cac3a87059
novakvova <novakvova@gmail.com>
2021-06-08 17:26:22 +0300

    Begin upload images

---
+++ cars/add.php
@@ -0,0 +1,55 @@
+<?php include "../_head.php"; ?>
+
+
+<div class="container">
+    <div class="p-3">
+        <h2>Додати автомобіль</h2>
+        <form  method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Назва</label>
+                <?php
+                echo "<input type='text' name='name' class='form-control' id='exampleInputEmail1'
+                           placeholder='Enter animal name'>"
+                ?>
+            </div>
+
+            <div class="form-group">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="card" style="width: 100%;">
+                                <div class="card-body">
+                                    <img src="/uploads/no-profile-photo.jpg"
+                                         style="width:100%; cursor: pointer;"
+                                         id="uploadImage"
+                                         alt="hello">
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-2">Додати</button>
+        </form>
+    </div>
+</div>
+
+<?php include "../_footer.php"; ?>
+
+<script>
+
+    $(function() {
+        var uploader;
+       $("#uploadImage").on("click", function() {
+           if (uploader) {
+               uploader.remove();
+           }
+           uploader = $('<input type="file" name="workImage" accept="image/* style="display:none"/>');
+           uploader.click();
+       });
+    });
+</script>
